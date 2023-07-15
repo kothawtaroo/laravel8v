1fe70f0cd21e1db2fffa3b3e0883787a4e0f50c6
TTO <myatthuta2019@gmail.com>
2023-07-15 20:41:33 +0630

    edit css

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,13 +8,13 @@
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="{{ asset (' assets/css/animate.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{ asset (' assets/css/font-awesome.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{ asset (' assets/css/bootstrap.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{ asset (' assets/css/owl.carousel.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{ asset (' assets/css/chosen.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{ asset (' assets/css/style.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{ asset (' assets/css/color-01.css')}}">
+	<link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/chosen.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/color-01.css">
     @livewireStyles
 </head>
 <body class="home-page home-01 ">
@@ -440,15 +440,15 @@
 		</div>
 	</footer>
 	
-	<script src="{{ asset ('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
-	<script src="{{ asset ('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
-	<script src="{{ asset ('assets/js/bootstrap.min.js')}}"></script>
-	<script src="{{ asset ('assets/js/jquery.flexslider.js')}}"></script>
-	<script src="{{ asset ('assets/js/chosen.jquery.min.js')}}"></script>
-	<script src="{{ asset ('assets/js/owl.carousel.min.js')}}"></script>
-	<script src="{{ asset ('assets/js/jquery.countdown.min.js')}}"></script>
-	<script src="{{ asset ('assets/js/jquery.sticky.js')}}"></script>
-	<script src="{{ asset ('assets/js/functions.js')}}"></script>
+	<script src="assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4"></script>
+	<script src="assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.flexslider.js"></script>
+	<script src="assets/js/chosen.jquery.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/jquery.countdown.min.js"></script>
+	<script src="assets/js/jquery.sticky.js"></script>
+	<script src="assets/js/functions.js"></script>
     @livewireScripts
 </body>
 </html>
